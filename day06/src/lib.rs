5333509739e0585bc50565aaf1602e3bf4c30383
digital <jcburnside97@gmail.com>
2021-12-06 09:57:13 -0500

    optimized day 6

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -1,68 +1,42 @@
-use std::{str::FromStr, collections::HashMap};
+use std::str::FromStr;
 
-use itertools::Itertools;
+type Fish = [usize;9];
 
-#[derive(Clone, Copy,PartialEq, Eq, PartialOrd, Ord)]
-struct Fish(u8);
-
-impl Fish {
-    fn new() -> Self {
-        Self(8)
+fn parse(input:&str) -> Fish {
+    let mut output = [0;9];
+    for fish in input.split(',').map(<u8 as FromStr>::from_str).map(Result::unwrap) {
+        output[fish as usize] += 1;
     }
-
-    fn next_day(&mut self) -> Option<Self> {
-        if self.0 == 0 {
-            self.0 = 6;
-            Some(Self::new())
-        } else {
-            self.0 -= 1;
-            None
-        }
-    }
+    output
 }
 
-fn parse(input:&str) -> Vec<Fish> {
-    input.split(',').map(<u8 as FromStr>::from_str).map(Result::unwrap).map(Fish).collect()
-}
-
-fn run_for(mut fish:Vec<Fish>,days:usize) -> usize {
+fn run_for(mut fish:Fish,days:usize) -> usize {
+    let mut swap = [0;9];
     for _ in 0..days {
-        let mut to_add = Vec::new();
-        for fish in fish.iter_mut() {
-            if let Some(fish) = fish.next_day() {
-                to_add.push(fish);
-            }
-        }
-        fish.append(&mut to_add);
-    }
-
-    fish.len()
-}
-
-fn part1(fish:Vec<Fish>) -> usize { 
-    run_for(fish, 80)
-}
-
-fn part2(fish:Vec<Fish>) -> usize {
-    let mut map = HashMap::<u8,usize>::from_iter(fish.iter().sorted().group_by(|f| f.0).into_iter().map(|(day,fish)| (day,fish.count())));
-    let mut swap : HashMap<u8,usize> = HashMap::with_capacity(9);
-    for _ in 0..256 {
-        for (group,count) in map.iter().sorted_by_key(|(day,_)| -(**day as i8)){
+        for (group,count) in fish.iter().enumerate(){
             match group {
                 0=> {
-                    *swap.entry(8).or_default() = *count;
-                    *swap.entry(6).or_default() += *count;
+                    swap[8]=*count;
+                    swap[6]+=count;
                 }
                 1..=8 => {
-                    *swap.entry( *group - 1).or_default() += count;
+                    swap[group-1] += count;
                 }
                 _=>unreachable!()
             }
         }
-        map = swap;
-        swap = HashMap::with_capacity(9);
+        fish = swap;
+        swap = [0;9];
     }
-    map.values().sum()
+    fish.iter().sum()
+}
+
+fn part1(fish:Fish) -> usize { 
+    run_for(fish, 80)
+}
+
+fn part2(fish:Fish) -> usize {
+    run_for(fish, 256)
 }
 
 pub fn run() {
