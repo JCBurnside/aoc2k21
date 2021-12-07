d9da02eafa7191c131600303c9dd1cb02fa3ecde
digital <jcburnside97@gmail.com>
2021-12-07 10:35:12 -0500

    day07 complete

---
+++ day07/src/lib.rs
@@ -0,0 +1,40 @@
+use std::str::FromStr;
+use itertools::Itertools;
+
+fn part1(data:&[i32]) -> i32 {
+    let median = data.iter().sorted().nth(data.len()/2).unwrap();
+    data.iter().map(|crab| (crab - median).abs()).sum()
+}
+
+fn part2(data:&[i32]) -> i32 {
+    let mean = data.iter().map(|f| *f as f32).sum::<f32>() / data.len() as f32;
+    std::cmp::min(data.iter().map(|crab| (1i32..=(crab-mean.floor() as i32).abs()).sum::<i32>()).sum(),
+    data.iter().map(|crab| (1i32..=(crab-mean.ceil() as i32).abs()).sum::<i32>()).sum())
+}
+
+fn parse(input : &str)->Vec<i32> {
+    input.split(',').map(<i32 as FromStr>::from_str).map(Result::unwrap).collect()
+}
+
+pub fn run() {
+    println!("day 07:");
+    let data = parse(include_str!("input.txt"));
+    println!("part 1: {}", part1(&data));
+    println!("part 2: {}", part2(&data));
+}
+
+#[cfg(test)]
+mod tests {
+    const TEST : &'static str = "16,1,2,0,4,2,7,1,2,14";
+    #[test]
+    fn part1() {
+        let data = super::parse(TEST);
+        assert_eq!(37,super::part1(&data));
+    }
+
+    #[test]
+    fn part2() {
+        let data = super::parse(TEST);
+        assert_eq!(168,super::part2(&data));
+    }
+}
--- runner/src/main.rs
+++ runner/src/main.rs
@@ -5,4 +5,5 @@ fn main() {
     day04::run();
     day05::run();
     day06::run();
+    day07::run();
 }
